a12ee575533f3f4a8396b5c31483e7fbbf1fc6af
Zakaria Moumen <zmoumen@c2r4p9.1337.ma>
2022-11-21 23:29:14 +0100

    bonus fix

--- bonus_setup.sh
+++ bonus_setup.sh
@@ -1,31 +1,36 @@
 vg_oldname=$(vgdisplay | grep "VG Name" | awk '{printf "%s", $3}')
 mapper_oldvgname=$(echo $vg_oldname | sed "s/-/--/g")
+
 echo "----setup var lv----"
 lvcreate -L 3G -n var $vg_oldname
 mkfs -t ext4 /dev/$vg_oldname/var
 mkdir /mnt/var && mount /dev/$vg_oldname/var /mnt/var
-mv /var/* /mnt/var && mount /dev/$vg_oldname/var /var
+mv /var/* /mnt/var && mount -o bind /mnt/var /var
+umount /mnt/var && rm -r /mnt/var
 echo "/dev/$vg_oldname/var	/var	ext4	defaults	0	2" >> /etc/fstab
 
 echo "----setup srv lv-----"
 lvcreate -L 3G -n srv $vg_oldname
 mkfs -t ext4 /dev/$vg_oldname/srv
 mkdir /mnt/srv && mount /dev/$vg_oldname/srv /mnt/srv
-mv /srv/* /mnt/srv && mount /dev/$vg_oldname/srv /srv
+mv /srv/* /mnt/srv && mount -o bind /mnt/srv /srv
+umount /mnt/srv && rm -r /mnt/srv
 echo "/dev/$vg_oldname/srv	/srv	ext4	defaults	0	2" >> /etc/fstab
 
 echo "----setup tmp lv-----"
 lvcreate -L 3G -n tmp $vg_oldname
 mkfs -t ext4 /dev/$vg_oldname/tmp
 mkdir /mnt/tmp && mount /dev/$vg_oldname/tmp /mnt/tmp
-mv /tmp/* /mnt/tmp && mount /dev/$vg_oldname/tmp /tmp 
+mv /tmp/* /mnt/tmp && mount -o bind /mnt/tmp /tmp
+umount /mnt/tmp && rm -r /mnt/tmp
 echo "/dev/$vg_oldname/tmp	/tmp	ext4	defaults	0	2" >> /etc/fstab
 
 echo "----setup var/log lv----"
 lvcreate -L 3G -n var-log $vg_oldname
 mkfs -t ext4 /dev/$vg_oldname/var-log
 mkdir /mnt/varlog && mount /dev/$vg_oldname/var-log /mnt/varlog
-mv /var/log/* /mnt/varlog && mount /dev/$vg_oldname/var-log /var/log
+mv /var/log/* /mnt/varlog && mount -o bind /mnt/varlog /var/log
+umount /mnt/varlog && rm -r /mnt/varlog
 echo "/dev/$vg_oldname/var-log	/var/log	ext4	defaults	0	2" >> /etc/fstab
 
 echo "extend /home / SWAP"
