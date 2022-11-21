5910d6b1879a3986f401906f1116e357b1b0874a
Zakaria Moumen <zmoumen@c2r4p9.1337.ma>
2022-11-21 21:35:03 +0100

    mandatory fix

--- mandatory_setup.sh
+++ mandatory_setup.sh
@@ -4,31 +4,38 @@
 
 apt update
 apt upgrade
-apt install ssh
+
+adduser zmoumen 2> /dev/null
+
+addgroup user42
+adduser zmoumen user42
+
+apt install ssh -y
+
 sed -i "s/#Port.*/Port 4242/" /etc/ssh/sshd_config
 sed -i "s/#PermitRootLogin.*/PermitRootLogin no/" /etc/ssh/sshd_config
+systemctl restart sshd.service
+
 
 hst_hld=$(hostname)
 hostnamectl set-hostname zmoumen42
-sed -i "s/$hst/zmoumen42/" /ets/hosts
+sed -i "s/$hst_hld/zmoumen42/" /ets/hosts
 
-apt install sudo
-addgroup user42 
-adduser zmoumen user42
-adduser zmoumen sudo
 
 sed -i "s/PASS_MAX_DAYS.*/PASS_MAX_DAYS	30/" /etc/login.defs
 sed -i "s/PASS_MIN_DAYS.*/PASS_MIN_DAYS	2/" /etc/login.defs
 sed -i "s/PASS_WARN_AGE.*/PASS_WARN_AGE	7/" /etc/login.defs
 
-apt install libpam-pwquality
+apt install libpam-pwquality -y
 sed -i "s/pam_pwquality.so.*/pam_pwquality.so	enforce_for_root minlen=10 ucredit=-1 lcredit=-1 dcredit=-1 maxsequence=3 usercheck=1 difok=7/" /etc/pam.d/common-password
 
-apt install ufw
+apt install ufw -y
 ufw allow 4242
 ufw enable
 
 mkdir /var/log/sudo 2> /dev/null
+
+apt install sudo -y
 
 echo "\
 #born2beroot requirements
@@ -38,4 +45,22 @@ Defaults	log_input,log_output
 Defaults	iolog_dir=\"/var/log/sudo/%{command}/%{user}/%{seq}\"
 Defaults	logfile=\"/var/log/sudo/sudo.log\"
 Defaults	passwd_tries=3
-Defaults	secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin\"" > /etc/sudoers.d/b2br.sudoers
+Defaults	secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin\"" >> /etc/sudoers
+
+
+adduser zmoumen sudo
+
+wget https://raw.githubusercontent.com/k34n4y138/born2beroot/master/monitoring.sh -O /monitoring.sh
+
+crontab "*/10 * * * *" /monitoring.sh
+
+clear
+lsblk
+
+echo "------------------"
+
+ufw status
+
+echo "--------------------"
+
+crontab -l
