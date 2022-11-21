fca7e6b73f03bdad33e32e8998e9d8d52451484e
Zakaria Moumen <zmoumen@c2r4p9.1337.ma>
2022-11-21 19:51:18 +0100

    mandatory fix

--- mandatory_setup.sh
+++ mandatory_setup.sh
@@ -37,5 +37,5 @@ Defaults	log_input,log_output
 Defaults	iolog_dir=\"/var/log/sudo/%{command}/%{user}/%{seq}\"
 Defaults	logfile=\"/var/log/sudo/sudo.log\"
 Defaults	passwd_tries=3
-Defaults	secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin\" > /etc/sudoers.d/b2br.sudoers
+Defaults	secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin\"" > /etc/sudoers.d/b2br.sudoers
 
