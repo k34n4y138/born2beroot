b58d6c2074c1a1f29e915d69ab285118947c4b69
Zakaria Moumen <zmoumen@c2r4p9.1337.ma>
2022-11-21 19:49:29 +0100

    mandatory fix

--- mandatory_setup.sh
+++ mandatory_setup.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+# you should have passed the installation process
+
 apt update
 apt upgrade
-
+apt install ssh
 sed -i "s/#Port.*/Port 4242/" /etc/ssh/sshd_config
 sed -i "s/#PermitRootLogin.*/PermitRootLogin no/" /etc/ssh/sshd_config
 
+hst_hld=$(hostname)
 hostnamectl set-hostname zmoumen42
-hostnamectl
+sed -i "s/$hst/zmoumen42/" /ets/hosts
+
 apt install sudo
 addgroup user42 
 adduser zmoumen user42
 adduser zmoumen sudo
-sed -i "s/PASS_MAX_DAYS.*/PASS_MAX_DAYS	30/" /ect/login.defs
+
+sed -i "s/PASS_MAX_DAYS.*/PASS_MAX_DAYS	30/" /etc/login.defs
 sed -i "s/PASS_MIN_DAYS.*/PASS_MIN_DAYS	2/" /etc/login.defs
 sed -i "s/PASS_WARN_AGE.*/PASS_WARN_AGE	7/" /etc/login.defs
 
 echo "password	requisite	pam_pwquality.so	enforce_for_root minlen=10 ucredit=-1 lcredit=-1 dcredit=-1 maxsequence=3 usercheck=1 difok=7" >> /etc/pam.d/common-password
 
+apt install ufw
 ufw allow 4242
 ufw enable
 
-
-
-touch /etc/sudoers.d/b2br.sudoers
-
-mkdir /var/log/sudo
+mkdir /var/log/sudo 2> /dev/null
 
 echo "\
 #born2beroot requirements
